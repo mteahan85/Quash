8db0ac5d2ecb9d864339c63c2e18a8b270544396
Megan <mteahan@ku.edu>
2015-03-02 21:10:45 -0600

    clean up

--- main.c
+++ main.c
@@ -47,51 +47,49 @@ int main(int argc, char **argv, char **envp)
   
 }
 
-//ChangeDirectories
+//Change Directories (cd)
 void cd(const char *dir){
   
   if(dir == NULL){ //nothing is passed in -- understood "HOME"
-    if(chdir(getenv("HOME")) == -1){
+    if(chdir(getenv("HOME")) == -1){ //checks for HOME directory
       printf("<%s> is not a working pathname.\n", strerror(errno));
     }
   }else{
-    if(chdir(dir) == -1){
+    if(chdir(dir) == -1){ //checks for directory given
       printf("<%s> is not a working pathname.\n", strerror(errno));
     }
   }  
 }
 
-
+//List items within current directory (ls)
 void ls(){ //will eventually need this to potentially return a char*
   
-    char* cwd;
-    char buf[BSIZE];
-
-    cwd = getcwd( buf, BSIZE ); //current file directory
+  char* cwd;
+  char buf[BSIZE];
   
+  cwd = getcwd( buf, BSIZE ); //current file directory
   
   DIR *dir; 
   struct dirent *dirFiles;
   dir = opendir(cwd);
   if (dir)
   {
-    while ((dirFiles = readdir(dir)) != NULL)
+    while ((dirFiles = readdir(dir)) != NULL) //reads files with in that directory
     {
       printf("%s\n", dirFiles->d_name);
     }
-
     closedir(dir);
   }
 }  
+
+//Displays jobs when user calls jobs function
+void jobDisplay(){
+  for (int i = 0; i < BSIZE; i++){
+    if (jobs[i].alive){
+      printf("[%d]\t%d\t%s\n",jobs[i].id,jobs[i].pid,jobs[i].command);
+    }
+  } 
   
-
-void jobs(){
-    for (int i = 0; i < BSIZE; i++){
-      if (jobs[i].alive){
-	printf("[%d]\t%d\t%s\n",jobs[i].id,jobs[i].pid,jobs[i].command);
-      }
-    } 
-
 }
 
 //will run command in background
